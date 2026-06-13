6fd30b65bb8391285f1414b6a3764604e2096c8d
kywill18 <williams.kyla05@gmail.com>
2026-06-12 22:14:40 -0400

    Doing some refactoring in the VinylCollection class (Done with addVinyl working on removeVinyl)

--- main/src/VinylCollection.java
+++ main/src/VinylCollection.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class VinylCollection {
+    private ArrayList<Vinyl> wantList = new ArrayList<>();
+    private ArrayList<Vinyl> haveList = new ArrayList<>();
+    private ArrayList<Vinyl> favList = new ArrayList<>();
+
+    public ArrayList<Vinyl> getWantList() { return wantList; }
+    public ArrayList<Vinyl> getHaveList() { return haveList; }
+    public ArrayList<Vinyl> getFavList() { return favList; }
+
+    public void addVinyl(Vinyl vinyl){
+        if (vinyl.getStatus().equals("Want")){
+            wantList.add(vinyl);
+        }
+
+        else if (vinyl.getStatus().equals("Have")){
+            haveList.add(vinyl);
+        }
+
+        else if (vinyl.getStatus().equals("Favorite")){
+            favList.add(vinyl);
+        }
+    }
+
+    public void removeVinyl(Vinyl vinyl){
+
+    }
 }
